d8120779d90841d43a16f44237dc3e8db7a214ec
825814 <120297399+825814@users.noreply.github.com>
2023-01-14 14:47:42 +0000

    [finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,11 +4,16 @@ class ListsController < ApplicationController
   end
   
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
     # 投稿機能から詳細画面へリダイレクト
+    else
+      render :new
+    end
   end
+  
+  
 
   def index
     @lists = List.all
